dd53221c8d57391f7727cfafa43015ab80f03d16
Paul Bellamy <paul.a.bellamy@gmail.com>
2012-06-05 18:22:01 +0100

    Using a channel as our lock is 2x as fast as a Mutex (Fixes #1)

--- ratecounter.go
+++ ratecounter.go
@@ -6,35 +6,35 @@ package ratecounter
 
 import (
 	"time"
-  "sync"
 )
 
 // A Counter is a thread-safe counter implementation
 type Counter struct {
 	value int64
-  mutex *sync.Mutex
+	lock  chan int
 }
 
 // NewCounter is used to construct a new Counter object
 func NewCounter() *Counter {
 	return &Counter{
 		value: 0,
-    mutex: &sync.Mutex{},
+		lock:  make(chan int, 1),
 	}
 }
 
 // Increment the counter by some value
 func (c *Counter) Incr(val int64) {
-  c.mutex.Lock()
-  defer c.mutex.Unlock()
-  c.value += val
+	c.lock <- 1
+	c.value += val
+	<-c.lock
 }
 
 // Return the counter's current value
 func (c *Counter) Value() int64 {
-  c.mutex.Lock()
-  defer c.mutex.Unlock()
-  return c.value
+	c.lock <- 1
+	val := c.value
+	<-c.lock
+	return val
 }
 
 // A RateCounter is a thread-safe counter which returns the number of times
--- ratecounter_test.go
+++ ratecounter_test.go
@@ -5,67 +5,67 @@
 package ratecounter
 
 import (
-  "sync"
-  "testing"
-  "time"
+	"sync"
+	"testing"
+	"time"
 )
 
 func TestCounter(t *testing.T) {
-  c := NewCounter()
+	c := NewCounter()
 
-  check := func(expected int64) {
-    val := c.Value()
-    if val != expected {
-      t.Error("Expected ",val," to equal ",expected)
-    }
-  }
+	check := func(expected int64) {
+		val := c.Value()
+		if val != expected {
+			t.Error("Expected ", val, " to equal ", expected)
+		}
+	}
 
-  check(0)
-  c.Incr(1)
-  check(1)
-  c.Incr(9)
-  check(10)
+	check(0)
+	c.Incr(1)
+	check(1)
+	c.Incr(9)
+	check(10)
 
-  // Concurrent usage
-  wg := &sync.WaitGroup{}
-  wg.Add(3)
-  for i := 1; i <= 3; i++ {
-    go func(val int64) {
-      c.Incr(val)
-      wg.Done()
-    }(int64(i))
-  }
-  wg.Wait()
-  check(16)
+	// Concurrent usage
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+	for i := 1; i <= 3; i++ {
+		go func(val int64) {
+			c.Incr(val)
+			wg.Done()
+		}(int64(i))
+	}
+	wg.Wait()
+	check(16)
 }
 
 func TestRateCounter(t *testing.T) {
-  interval := 500 * time.Millisecond
-  r := NewRateCounter(interval)
+	interval := 500 * time.Millisecond
+	r := NewRateCounter(interval)
 
-  check := func(expected int64) {
-    val := r.Rate()
-    if val != expected {
-      t.Error("Expected ",val," to equal ",expected)
-    }
-  }
+	check := func(expected int64) {
+		val := r.Rate()
+		if val != expected {
+			t.Error("Expected ", val, " to equal ", expected)
+		}
+	}
 
-  check(0)
-  r.Mark()
-  check(1)
-  r.Mark()
-  r.Mark()
-  check(3)
-  time.Sleep(2 * interval)
-  check(0)
+	check(0)
+	r.Mark()
+	check(1)
+	r.Mark()
+	r.Mark()
+	check(3)
+	time.Sleep(2 * interval)
+	check(0)
 }
 
 func BenchmarkRateCounter(b *testing.B) {
-  interval := 0 * time.Millisecond
-  r := NewRateCounter(interval)
+	interval := 0 * time.Millisecond
+	r := NewRateCounter(interval)
 
-  for i := 0; i < b.N; i++ {
-    r.Mark()
-    r.Rate()
-  }
+	for i := 0; i < b.N; i++ {
+		r.Mark()
+		r.Rate()
+	}
 }
