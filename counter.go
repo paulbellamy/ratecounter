be2669ec82baf9b7aed7ef9a69afba688c570f3e
Alexander <cheshir@programist.ru>
2017-07-18 22:59:13 +0300

    Added badges, fixed golint recomendations

--- counter.go
+++ counter.go
@@ -5,16 +5,17 @@ import "sync/atomic"
 // A Counter is a thread-safe counter implementation
 type Counter int64
 
-// Increment the counter by some value
+// Incr method increments the counter by some value
 func (c *Counter) Incr(val int64) {
 	atomic.AddInt64((*int64)(c), val)
 }
 
+// Reset method resets the counter's value to zero
 func (c *Counter) Reset() {
 	atomic.StoreInt64((*int64)(c), 0)
 }
 
-// Return the counter's current value
+// Value method returns the counter's current value
 func (c *Counter) Value() int64 {
 	return atomic.LoadInt64((*int64)(c))
 }
